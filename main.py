831486e1a83ce0f113ee2ab4aacc03f52983e6b3
theosassan <103321888+theosassan@users.noreply.github.com>
2022-08-08 21:16:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ token = os.getenv("TOKEN")
 @client.event
 async def on_ready():
   print("hi")
+  await sleep(3000)
   await rolls.start()
   
 @client.command()
@@ -19,30 +20,44 @@ async def abuse(ctx):
     await sleep(1)
 
 @client.event
-async def on_message(message):
-    if message.author.id == 432610292342587392:
-      try:
-        async for message in message.channel.history(limit=1):
-          embed = message.embeds[0]
-          msgdict = embed.to_dict()
-          print(msgdict)
-          name = msgdict['author']['name']
-          claims = msgdict['description']
-          claims = claims.split('Claims: #')[1]
-          claims = int(claims.split('\n')[0])
-          print(claims)
-          print(name)
-          emoji = '❤️'
-          if claims <= 500:
-            await message.add_reaction(emoji)
-      except:
-        pass
+#async def on_message(message):
+#    if message.author.id == 432610292342587392:
+#      try:
+#        async for message in message.channel.history(limit=1):
+#          embed = message.embeds[0]
+#          msgdict = embed.to_dict()
+#          print(msgdict)
+#          name = msgdict['author']['name']
+#          claims = msgdict['description']
+#          claims = claims.split('Claims: #')[1]
+#          claims = int(claims.split('\n')[0])
+#          print(claims)
+#          print(name)
+#          emoji = '❤️'
+#          if claims <= 500:
+#            await message.add_reaction(emoji)
+#      except:
+#        pass
 
 @tasks.loop(hours = 1)
 async def rolls():
   channel = client.get_channel(766491456885227550)
   for _ in range(10):
     await channel.send("$wa")
+    async for message in channel.history(limit=1):
+      embed = message.embeds[0]
+      msgdict = embed.to_dict()
+      print(msgdict)
+      name = msgdict['author']['name']
+      claims = msgdict['description']
+      claims = claims.split('Claims: #')[1]
+      claims = int(claims.split('\n')[0])
+      print(claims)
+      print(name)
+      emoji = '❤️'
+      if claims <= 500:
+        await message.add_reaction(emoji)
     await sleep(1)
+    
         
 client.run(token, bot = False)
